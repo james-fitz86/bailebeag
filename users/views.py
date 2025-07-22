cb353b31c3f75124e1474e0d5c7f0a36e110d1ce
james-fitz86 <james.fitzy86@gmail.com>
2025-07-22 18:48:26 +0100

    added views for register and profile

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
 
 # Create your views here.
+def register(request):
+  if request.method == 'POST':
+    form = UserRegisterForm(request.POST)
+    if form.is_valid():
+      form.save()
+      username = form.cleaned_data.get('username')
+      return redirect('home')
+  else:
+    form = UserRegisterForm()
+  return render(request, 'users/register.html', {'form': form} )
+
+@login_required
+def profile(request):
+  return render(request, 'users/profile.html')
