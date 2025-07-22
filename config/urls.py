c7d7d5fddd1120731f2d9ac5fbf93aea86741935
james-fitz86 <james.fitzy86@gmail.com>
2025-07-22 18:45:32 +0100

    added pathes for register, login, profile and logout

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,14 @@ from django.contrib import admin
 from django.urls import path
 
 from . import views
+from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name='home'),
+    path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('profile/', user_views.profile, name='profile'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
