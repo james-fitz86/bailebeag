d4aeb9c31e6b3f20023bf8cb5b45141daa583fbb
james-fitz86 <james.fitzy86@gmail.com>
2025-07-23 13:51:37 +0100

    Remove fallback DB URL from settings.py now that both of my machines use .env

--- config/settings.py
+++ config/settings.py
@@ -83,10 +83,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://jamesfitzpatrick:james@localhost:5433/gaateam',
-        conn_max_age=600
-    )
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 
