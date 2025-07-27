c2225333ce5d9351a66788cdd7b5a7e3d0aa44a7
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-07-27 20:50:38 +0100

    added redirect for authenicated users for the login path

--- config/urls.py
+++ config/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('register/', user_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html', redirect_authenticated_user=True), name='login'),
     path('profile/', user_views.profile, name='profile'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
