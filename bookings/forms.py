58e8e0c0f103044599aab4642164741ab6a51a48
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-07-31 20:25:55 +0100

    added booking form for pitch bookings

---
+++ bookings/forms.py
@@ -0,0 +1,46 @@
+from django import forms
+from .models import Booking, Pitch
+import datetime
+
+from django import forms
+from .models import Booking, Pitch
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ['pitch', 'name', 'email', 'phone', 'start_time', 'end_time', 'method']
+        widgets = {
+            'start_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'end_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+
+        # Restricts pitch options based on user
+        if user and user.is_authenticated:
+            role = user.role
+            if role in ['coach', 'chairman', 'secretary', 'manager']:
+                self.fields['pitch'].queryset = Pitch.objects.all()
+            else:
+                self.fields['pitch'].queryset = Pitch.objects.none()
+
+            # Restricts method choice only to manager, all other users are default web in views, so don't need this choice
+            if role == 'manager':
+                self.fields['method'].choices = [
+                    ('phone', 'Phone'),
+                    ('email', 'Email'),
+                ]
+            else:
+                self.fields['method'].widget = forms.HiddenInput()
+                self.fields['method'].initial = 'web'
+
+            for field in ['name', 'email', 'phone']:
+                self.fields[field].required = False
+                self.fields[field].widget = forms.HiddenInput()
+        else:
+            self.fields['method'].widget = forms.HiddenInput()
+            self.fields['method'].initial = 'web'
+            self.fields['pitch'].queryset = Pitch.objects.filter(name__icontains='Astro')
+
