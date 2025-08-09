3057cb5494cacb007f903e69c047a48503df90a2
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-08-09 18:14:39 +0100

    registered Pitch and Booking models to admin

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Pitch, Booking
 
 # Register your models here.
+@admin.register(Pitch)
+class PitchAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'is_public')
+    search_fields = ('name',)
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        'pitch', 'name', 'start_time', 'end_time', 'status', 'method', 'submitted_at'
+    )
+    list_filter = ('pitch', 'status', 'method', 'start_time')
+    search_fields = ('name', 'email', 'phone')
+    date_hierarchy = 'start_time'
+    ordering = ('-submitted_at',)
