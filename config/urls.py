3dc450c0545d8c08f07c2f5421f3f08d1120949e
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-08-12 13:29:37 +0100

    added urls for password reset

--- config/urls.py
+++ config/urls.py
@@ -47,5 +47,9 @@ urlpatterns = [
     path('teams/<int:pk>/delete/', team_views.TeamDeleteView.as_view(), name='team_delete'),
     path('notifications/', alert_views.NotificationListView.as_view(), name='notification_list'),
     path('notifications/<int:pk>/', alert_views.NotificationDetailView.as_view(), name='notification_detail'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html', html_email_template_name='users/emails/password_reset_email.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 
 ]
