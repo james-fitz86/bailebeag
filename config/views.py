abb63a9556a3b21c4b49c8cca90b0ef88319e303
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-08-16 16:35:39 +0100

    added views for error templates

--- config/views.py
+++ config/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
   return render(request, 'config/home.html')
+
+def error_403(request, exception=None):
+    return render(request, 'config/errors/403.html', status=403)
+
+def error_404(request, exception=None):
+    return render(request, 'config/errors/404.html', status=404)
+
+def error_500(request):
+    return render(request, 'config/errors/500.html', status=500)
